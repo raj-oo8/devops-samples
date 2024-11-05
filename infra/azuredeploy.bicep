75da04af9fd7c8cbd9dc481d102099dfab80e935
Raj <rj.sse8@outlook.com>
2024-11-05 19:04:30 +0530

    Enhance Azure deployment workflows by adding location and SKU parameters for static sites

--- infra/azuredeploy.bicep
+++ infra/azuredeploy.bicep
@@ -1,10 +1,12 @@
 param staticSites_name string = 'azure-static-web-app'
+param staticSites_location string = 'East Asia'
+param staticSites_sku string = 'Free'
 
 resource staticSite 'Microsoft.Web/staticSites@2023-01-01' = {
   name: staticSites_name
-  location: 'East Asia'
+  location: staticSites_location
   sku: {
-    name: 'Free'
+    name: staticSites_sku
   }
   properties: {
   }
--- infra/azuredeploy.tf
+++ infra/azuredeploy.tf
@@ -7,20 +7,26 @@ variable "subscription_id" {
   description = "The subscription ID"
   type        = string
 }
-
 variable "staticSites_name" {
   type        = string
   default     = "azure-static-web-app"
 }
-
 variable "resource_group_name" {
   description = "The name of the resource group"
+  type        = string
+}
+variable "location" {
+  description = "The location for the resource group"
+  type        = string
+}
+variable "sku_tier" {
+  description = "The SKU tier for the static web app"
   type        = string
 }
 
 resource "azurerm_static_web_app" "example" {
   name                = var.staticSites_name
-  location            = "East Asia"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  sku_tier            = "Free"
+  sku_tier            = var.sku_tier
 }
